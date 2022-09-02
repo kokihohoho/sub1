c1f41588b370038332a80f333aa732b6a381e970
kokihohoho <111342404+kokihohoho@users.noreply.github.com>
2022-09-02 13:26:07 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,37 @@
+$(function(){
+    $('.wrap a').click(function(){
+        $('.pop').fadeIn();//.show()
+    });
+
+    $('.pop a').click(function(){
+        $('.pop').fadeOut();//.hide()
+    });
+
+
+    $('.wrap1 a').click(function(){
+        $('.pop1').fadeIn();//.show()
+    });
+
+    $('.pop1 a').click(function(){
+        $('.pop1').fadeOut();//.hide()
+    });
+
+
+    $('.wrap2 a').click(function(){
+        $('.pop2').fadeIn();//.show()
+    });
+
+    $('.pop2 a').click(function(){
+        $('.pop2').fadeOut();//.hide()
+    });
+
+
+    $('.wrap3 a').click(function(){
+        $('.pop3').fadeIn();//.show()
+    });
+
+    $('.pop3 a').click(function(){
+        $('.pop3').fadeOut();//.hide()
+    });
+
+});
